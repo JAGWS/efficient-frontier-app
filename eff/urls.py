068cfaba8e26aca8f99a2e7c4bae63fda86f19a8
JAGWS <jose.guzman@wealthsolutions.com>
2021-09-03 11:15:09 +0200

    Modify welcome page, add upload.html, styles..:

--- eff/urls.py
+++ eff/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 
 from . import views
 
-app_name = 'eff'
+# app_name = 'eff'
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.upload, name="upload"),
+    path("display", views.display, name="display")
 ]
--- eff/views.py
+++ eff/views.py
@@ -1,6 +1,31 @@
+from django.http.response import HttpResponse, HttpResponseBase, HttpResponseRedirect
 from django.shortcuts import render
+from django import forms
+
+
+class UploadInfoActivosForm(forms.Form):
+    title = forms.CharField(max_length=50)
+    excel = forms.FileField()
 
 
 # Create your views here.
-def index(request):
-    return render(request, "eff/index.html")
+def upload(request):
+    if request.method == "POST":
+        form = UploadInfoActivosForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            # Process excel --> save data to model --> show filters and rest of elements
+
+            return HttpResponseRedirect("display", request)
+        else:
+            return render(request, "eff/upload.html", context={ 
+                "form": form
+            })
+
+    return render(request, "eff/upload.html", context={
+        "form": UploadInfoActivosForm()
+    })
+
+
+def display(request):
+    return render(request, "eff/display.html")
